6a761756f41fd2f4b5fa4e136cbc26d598130676
itsllyaz <eliyasinbox1@gmail.com>
2026-09-07 00:47:53 +0300

    simple server

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main(){
-	fmt.Println("hello")
+	fmt.Println("....")
+
+	server := http.Server{
+		Addr: ":8080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+			fmt.Fprintln(w, "Hello from server...")
+		}),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil{
+		log.Fatalf("THE ERROR: :%v", err)
+	}
 }
