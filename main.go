8e4f957f424cd1e13339df2764228e84f6f4ca64
itsllyaz <eliyasinbox1@gmail.com>
2026-09-07 11:40:06 +0300

    custom server handler

--- main.go
+++ main.go
@@ -11,13 +11,28 @@ func helloHandler(w http.ResponseWriter, r * http.Request){
 }
 
 
+func readinessHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	w.Write([]byte("Server working..."))
+
+}
 
 func main(){
 	fmt.Println("....")
 	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("."))
-	mux.HandleFunc("/bob", helloHandler)
-	mux.Handle("/hello", fs)
+	image_fs := http.FileServer(http.Dir("./images"))	
+	mux.HandleFunc("/app/bob", helloHandler)
+
+	mux.Handle("/app/hello/", http.StripPrefix("/app/hello/", fs))
+	mux.Handle("/app/public/", fs)
+	
+	mux.Handle("/app/assets/", http.StripPrefix("/app/assets/", image_fs))
+
+	mux.HandleFunc("/app/healthz", readinessHandler)
+
 	server := &http.Server{
 		Addr: ":8080",
 		Handler: mux,  
