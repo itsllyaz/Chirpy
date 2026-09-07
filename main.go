44638cede42276e5fd6745a09cda62b2768293cf
itsllyaz <eliyasinbox1@gmail.com>
2026-09-07 10:19:51 +0300

    fileserver

--- main.go
+++ main.go
@@ -6,16 +6,22 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r * http.Request){
+	fmt.Fprintln(w, "Hello From server...")
+}
+
+
+
 func main(){
 	fmt.Println("....")
-
+	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir("."))
+	mux.HandleFunc("/bob", helloHandler)
+	mux.Handle("/hello", fs)
 	server := http.Server{
 		Addr: ":8080",
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-			fmt.Fprintln(w, "Hello from server...")
-		}),
+		Handler: mux,  
 	}
-
 	err := server.ListenAndServe()
 	if err != nil{
 		log.Fatalf("THE ERROR: :%v", err)
